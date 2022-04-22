3fd314e83819dcbcea6c1716159adb7c0fa6e185
DenisLuba <denislubapershins@gmail.com>
2022-04-22 15:43:23 +0300

    added enum EventType, classes CookedOrderEventDataRow, NoAvailableVideoEventDataRow, VideoSelectedEventDataRow

--- src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -5,6 +5,8 @@ import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
 public class StatisticManager {
     private static StatisticManager INSTANCE;
 
+    private StatisticManager() {}
+
     public static StatisticManager getInstance() {
         if (INSTANCE == null) INSTANCE = new StatisticManager();
         return INSTANCE;
