3546a44f4d1e4ad7ed615780cf27a8878e70a268
DenisLuba <denislubapershins@gmail.com>
2022-04-22 14:21:19 +0300

    add package statistic, statistic/event, class StatisticManager

---
+++ src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task27.task2712.statistic;
+
+import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+
+public class StatisticManager {
+    private static StatisticManager INSTANCE;
+
+    public static StatisticManager getInstance() {
+        if (INSTANCE == null) INSTANCE = new StatisticManager();
+        return INSTANCE;
+    }
+
+    public void register(EventDataRow data) {
+
+    }
+}
