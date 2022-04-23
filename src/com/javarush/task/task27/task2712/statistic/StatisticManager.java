1dbaa758f23a339dc770fa5c513e04bb74096a83
DenisLuba <delubis@mail.ru>
2022-04-23 22:09:37 +0300

    added inner class StatisticStorage in StatisticManager

--- src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,9 +1,16 @@
 package com.javarush.task.task27.task2712.statistic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+import com.javarush.task.task27.task2712.statistic.event.EventType;
 
 public class StatisticManager {
     private static StatisticManager INSTANCE;
+    private StatisticStorage statisticStorage = new StatisticStorage();
 
     private StatisticManager() {}
 
@@ -14,5 +21,15 @@ public class StatisticManager {
 
     public void register(EventDataRow data) {
 
+    }
+
+    private class StatisticStorage {
+        Map<EventType, List<EventDataRow>> storage;
+
+        public StatisticStorage() {
+            storage = new HashMap<>();
+            for (EventType eventType : EventType.values())
+                storage.put(eventType, new ArrayList<EventDataRow>());
+        }
     }
 }
